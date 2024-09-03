f70d1adee0da720bed75b54c4e01d32d4d10b32f
EgrShishov <e.shishov98@yandex.ru>
2024-09-03 11:37:28 +0300

    Lab_1

---
+++ WEB_253505_Bekarev/WEB_253505_Bekarev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_253505_Bekarev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
